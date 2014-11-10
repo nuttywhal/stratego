b46805ce518ba525c8f084851985674a67b410c4
nttran9 <nttran9@asu.edu>
2014-11-10 13:46:29 -0700

    added PieceType enumeration

---
+++ src/edu/asu/stratego/game/PieceType.java
@@ -0,0 +1,16 @@
+package edu.asu.stratego.game;
+
+public enum PieceType {
+    MARSHAL,
+    GENERAL,
+    COLONEL,
+    MAJOR,
+    CAPTAIN,
+    LIEUTENANT,
+    SERGEANT,
+    MINER,
+    SCOUT,
+    SPY,
+    BOMB,
+    FLAG;
+}
