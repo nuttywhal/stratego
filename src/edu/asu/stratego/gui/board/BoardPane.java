9d19539e0bd4010c168a56d0b877a2f2a29110bc
nttran9 <nttran9@asu.edu>
2014-11-12 13:32:58 -0700

    added the lakes to board constructor

--- src/edu/asu/stratego/gui/board/BoardPane.java
+++ src/edu/asu/stratego/gui/board/BoardPane.java
@@ -1,6 +1,7 @@
 package edu.asu.stratego.gui.board;
 
 import edu.asu.stratego.game.board.Board;
+import edu.asu.stratego.media.ImageConstants;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -20,5 +21,16 @@ public class BoardPane extends GridPane {
                 add(board.getSquare(row, col).getPane(), row, col);
             }
         }
+        
+        // Add the two lakes.
+        board.getSquare(2, 4).getPane().setPiece(ImageConstants.lake1_1);
+        board.getSquare(3, 4).getPane().setPiece(ImageConstants.lake1_2);
+        board.getSquare(2, 5).getPane().setPiece(ImageConstants.lake1_3);
+        board.getSquare(3, 5).getPane().setPiece(ImageConstants.lake1_4);
+        
+        board.getSquare(6, 4).getPane().setPiece(ImageConstants.lake2_1);
+        board.getSquare(7, 4).getPane().setPiece(ImageConstants.lake2_2);
+        board.getSquare(6, 5).getPane().setPiece(ImageConstants.lake2_3);
+        board.getSquare(7, 5).getPane().setPiece(ImageConstants.lake2_4);
     }
 }
